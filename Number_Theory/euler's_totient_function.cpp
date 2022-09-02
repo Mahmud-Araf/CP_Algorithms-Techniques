6a1a9dbad5f4ea2390fb0f040c03acac2493dba6
Mahmud-Araf <almahmudaraf@gmail.com>
2022-09-02 20:50:57 +0600

    added euler's_totient_function

---
+++ Number_Theory/euler's_totient_function.cpp
@@ -0,0 +1,33 @@
+//implementation of euler's totient/phi function
+
+/* Euler's phi function of n counts the positive integers up to n which are relatively prime to n.
+
+Time Complexity: O(log(n))
+*/
+
+
+int phi_function(int n)
+{
+   int result=n;
+   int temp=n;
+   for(int i=2;i*i<=n;i++)
+   {
+      if(temp%i==0)
+      {
+         while(temp%i==0)
+         {
+            temp/=i;
+         }
+         result*=(1.0-(1.0/(double)i));
+        
+      }
+   }
+  
+   if(temp>1)
+   {
+      result*=(1.0-((1.0/(double)temp)));
+      return result;
+   }
+   else
+   return result;
+}
